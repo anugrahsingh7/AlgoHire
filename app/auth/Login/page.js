2fd3d59f48b2bfd28129b20789223922f6362fcd
anugrahsingh07 <anugrahsingh7777@gmail.com>
2025-03-01 18:52:00 +0530

    login and sign up page updated

--- app/auth/Login/page.js
+++ app/auth/Login/page.js
@@ -7,6 +7,9 @@ import { useState } from "react";
 import { getSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { FcGoogle } from "react-icons/fc";
+import { ThemeProvider } from "@/app/Context/ThemeContext";
+import { ThemeToggle } from "@/app/_components/ThemeToggle";
 
 export default function Login() {
   const router = useRouter();
@@ -38,49 +41,55 @@ export default function Login() {
   };
 
   return (
-    <div className="flex w-full h-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+    <ThemeProvider>
+    <div className=" flex w-screen h-screen overflow-hiddenshadow-lg dark:bg-[#0f0f0f] justify-center  items-center bg-gray-100">
       <div
-        className="hidden bg-cover lg:block lg:w-1/2"
-        style={{
-          backgroundImage:
-            "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')",
-        }}
-      ></div>
+        className="hidden bg-cover lg:block lg:w-2/3">
+         <img
+                  className="w-full h-full object-cover"
+                  src="/photo2.jpeg"
+                  alt="AlgoHire"
+                />
+  
 
-      <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
-        <div className="flex justify-center mx-auto">
-          <Image
-            src="https://merakiui.com/images/logo.svg"
-            alt="Logo"
-            width={100}
-            height={30}
-            className="w-auto h-7 sm:h-8"
-          />
-        </div>
+      </div>
 
-        <p className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">
-          Welcome back!
+      <div className="w-full  px-6 py-8 md:px-8 lg:w-1/3 dark:bg-[#0f0f0f]">
+       
+
+        <div className="flex justify-between mx-auto items-center " >
+                <div className="flex items-center">
+                <img
+                  className="w-auto h-5 sm:h-6 me-1"
+                  src="/AlogHireIcon-removebg-preview.png"
+                  alt="AlgoHire"
+                />
+                <span className="text-2xl text-gray-800 dark:text-gray-200  font-semibold">AlgoHire</span>
+                </div>
+
+                <ThemeToggle/>
+              </div>
+
+        <p className="mt-4 text-xl text-left text-gray-600 dark:text-gray-300 ">
+          WELCOME BACK !
         </p>
 
-        <button 
+        {/* <button 
           onClick={handleGoogleSignIn}
-          className="flex items-center justify-center w-full mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+          className="flex items-center justify-center w-full mt-2 dark:text-gray-200 text-gray-800 transition-colors duration-300 transform border border-gray-300  dark:border-gray-700 hover:shadow-sm rounded-lg  hover:bg-gray-900 "
         >
-          <div className="px-4 py-2">
-            <svg className="w-6 h-6" viewBox="0 0 40 40"></svg>
-          </div>
-          <span className="w-5/6 px-4 py-3 font-bold text-center">
-            Sign in with Google
+          <span className="w-5/6 px-4 py-3 flex justify-center items-center font-bold text-center">
+          <FcGoogle className="me-1 text-2xl " /> Sign in with Google
           </span>
-        </button>
+        </button> */}
 
-        <div className="flex items-center justify-between mt-4">
+        {/* <div className="flex items-center justify-between mt-4">
           <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
           <span className="text-xs text-gray-500 uppercase dark:text-gray-400">
             or login with email
           </span>
           <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
-        </div>
+        </div> */}
 
         <form onSubmit={handleSubmit}>
           <div className="mt-4">
@@ -91,6 +100,7 @@ export default function Login() {
               name="email"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
               type="email"
+              placeholder="Enter your email"
             />
           </div>
 
@@ -110,30 +120,51 @@ export default function Login() {
               name="password"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
               type="password"
+              placeholder="Enter your password"
             />
           </div>
 
           <div className="mt-6">
             <button
               type="submit"
-              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+              className="w-full px-6 py-3 text-sm font-medium hover:shadow-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-lg hover:bg-green-500 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
             >
               Sign In
             </button>
           </div>
+          <div className="flex items-center justify-between mt-4">
+          <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+          <span className="text-xs text-gray-500 uppercase dark:text-gray-400">
+            or login with email
+          </span>
+          <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
+        </div>
+
+          <button 
+          onClick={handleGoogleSignIn}
+          className="flex items-center justify-center w-full mt-2 dark:text-gray-200 text-gray-800 transition-colors duration-300 transform border border-gray-300  dark:border-gray-700 hover:shadow-sm rounded-lg  hover:bg-gray-300 hover:dark:bg-gray-700 "
+        >
+          <span className="w-5/6 px-4 py-3 flex justify-center items-center font-bold text-center">
+          <FcGoogle className="me-1 text-2xl " /> Sign in with Google
+          </span>
+        </button>
         </form>
 
         <div className="flex items-center justify-between mt-4">
-          <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+          <span className="w-1/7 border-b dark:border-gray-600 md:w-1/6"></span>
           <a
             href="/auth/Signup"
             className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
           >
-            or sign up
+            or Don't have an account? Sign up
           </a>
-          <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+          <span className="w-1/7 border-b dark:border-gray-600 md:w-1/6"></span>
         </div>
+
+
+        
       </div>
     </div>
+    </ThemeProvider>
   );
 }
--- app/auth/Signup/page.js
+++ app/auth/Signup/page.js
@@ -4,6 +4,9 @@ import { registerUser } from "../../_lib/actions";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { redirect } from "next/navigation";
+import { ThemeProvider } from "@/app/Context/ThemeContext";
+import { ThemeToggle } from "@/app/_components/ThemeToggle";
+
 
 export default function Signup() {
   async function handleSubmit(event) {
@@ -20,124 +23,141 @@ export default function Signup() {
   }
 
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <ThemeProvider>
+    <section className="bg-gray-100 dark:bg-[#0f0f0f] ">
       <div className="flex justify-center min-h-screen">
+     
         <div
-          className="hidden bg-cover lg:block lg:w-2/5"
-          style={{
-            backgroundImage:
-              "url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fHx8&auto=format&fit=crop&w=715&q=80')",
-          }}
-        ></div>
+          className="hidden bg-cover lg:block lg:w-2/5">
+          <img
+          className="w-full h-full object-cover"
+          src="/photo2.jpeg"
+          alt="AlgoHire"
+        />
+        </div>
 
-        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5 dark:bg-[#0f0f0f]">
+        
           <div className="w-full">
-            <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
+            
+           <div className="flex justify-between items-center mb-2">
+           <div className="flex  items-center  " >
+                <img
+                  className="w-auto h-6 sm:h-7 me-1"
+                  src="/AlogHireIcon-removebg-preview.png"
+                  alt="AlgoHire"
+                />
+                <span className="text-3xl text-gray-800 dark:text-gray-200  font-semibold">AlgoHire</span>
+              </div>
+            
+            <ThemeToggle/></div>
+         
+            <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-gray-200">
               Get your free account now.
             </h1>
-            <p className="mt-4 text-gray-500 dark:text-gray-400">
+            <p className=" text-gray-600 dark:text-gray-400">
               Let’s get you all set up so you can verify your personal account
               and begin setting up your profile.
             </p>
 
-            <div className="mt-6">
-              <h1 className="text-gray-500 dark:text-gray-300">
-                Select type of account
-              </h1>
-              <div className="mt-3 md:flex md:items-center md:-mx-2">
-                <button className="flex justify-center w-full px-6 py-3 text-white bg-blue-500 rounded-lg md:w-auto md:mx-2 focus:outline-none">
-                  <span className="mx-2">Client</span>
-                </button>
-
-                <button className="flex justify-center w-full px-6 py-3 mt-4 text-blue-500 border border-blue-500 rounded-lg md:mt-0 md:w-auto md:mx-2 dark:border-blue-400 dark:text-blue-400 focus:outline-none">
-                  <span className="mx-2">Worker</span>
-                </button>
-              </div>
-            </div>
+            
 
             <form
               onSubmit={handleSubmit}
               className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2"
             >
               <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                <label className="block mb-1 text-sm text-gray-700 dark:text-gray-200">
                   First Name
                 </label>
                 <input
                   name="firstName"
                   type="text"
-                  placeholder="John"
-                  className="input-field"
+                  placeholder="Enter your first name"
+                  className="input-field px-3 py-1 rounded-md dark:bg-gray-800 border placeholder:text-sm text-md bg-gray-200 border-gray-300 dark:text-gray-200 dark:border-gray-600"
                 />
               </div>
 
               <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                <label className="block mb-1 text-sm text-gray-600 dark:text-gray-200">
                   Last Name
                 </label>
                 <input
                   name="lastName"
                   type="text"
-                  placeholder="Snow"
-                  className="input-field"
+                  placeholder="Enter your last name"
+                  className="input-field px-3 py-1 rounded-md dark:bg-gray-800 border placeholder:text-sm text-md bg-gray-200 border-gray-300 dark:text-gray-200 dark:border-gray-600"
                 />
               </div>
 
               <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                <label className="block mb-1 text-sm text-gray-600 dark:text-gray-200">
                   Phone Number
                 </label>
                 <input
                   name="phoneNumber"
-                  type="text"
-                  placeholder="XXX-XX-XXXX-XXX"
-                  className="input-field"
+                  type="number"
+                  pattern="[0-9]*"
+                  inputMode="numeric"
+                  placeholder="Enter your mobile no."
+                  className="[&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none input-field px-3 py-1 rounded-md dark:bg-gray-800 border placeholder:text-sm text-md bg-gray-200 border-gray-300 dark:text-gray-200 dark:border-gray-600"
                 />
               </div>
 
               <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                <label className="block mb-1 text-sm text-gray-600 dark:text-gray-200">
                   Email Address
                 </label>
                 <input
                   name="email"
                   type="email"
-                  placeholder="johnsnow@example.com"
-                  className="input-field"
+                  placeholder="Enter your email id"
+                  className="input-field px-3 py-1 rounded-md dark:bg-gray-800 border placeholder:text-sm text-md bg-gray-200 border-gray-300 dark:text-gray-200 dark:border-gray-600"
                 />
               </div>
 
               <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                <label className="block mb-1 text-sm text-gray-600 dark:text-gray-200">
                   Password
                 </label>
                 <input
                   name="password"
                   type="password"
                   placeholder="Enter your password"
-                  className="input-field"
+                  className="input-field px-3 py-1 rounded-md dark:bg-gray-800 border placeholder:text-sm text-md bg-gray-200 border-gray-300 dark:text-gray-200 dark:border-gray-600"
                 />
               </div>
 
               <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                <label className="block mb-1 text-sm text-gray-600 dark:text-gray-200">
                   Confirm Password
                 </label>
                 <input
                   name="confirmPassword"
                   type="password"
                   placeholder="Enter your password"
-                  className="input-field"
+                  className="input-field px-3 py-1 rounded-md dark:bg-gray-800 border placeholder:text-sm text-md bg-gray-200 border-gray-300 dark:text-gray-200 dark:border-gray-600"
                 />
               </div>
 
-              <button className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+              <button className="flex items-center justify-center w-1/3 px-2 py-2 text-md font-semibold tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 <span>Sign Up</span>
               </button>
             </form>
+            <div className="flex items-center justify-between mt-4">
+          <span className="w-1/4 border-b dark:border-gray-600 md:w-1/4"></span>
+          <a
+            href="/auth/Login"
+            className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+          >
+            or Already have an account? Log In
+          </a>
+          <span className="w-1/4 border-b dark:border-gray-600 md:w-1/4"></span>
+        </div>
           </div>
         </div>
       </div>
     </section>
+    </ThemeProvider>
   );
 }
